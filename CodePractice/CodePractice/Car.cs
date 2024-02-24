d20e1d52acb4004d49f5c436e289d2ac1fd54509
Shubham Kumar <2090944@cognizant.com>
2024-02-24 14:32:54 +0530

    Day 1 of Practice c# Code Car Example //Method call CTOR auto implementes if u calling

---
+++ CodePractice/CodePractice/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodePractice
+{
+    internal class Car
+    {
+        public Car()
+        {
+            Console.WriteLine("This is going to call Car ");
+        }
+        public void Drive()//Method
+        {
+            Console.WriteLine("Drive");
+
+
+        }
+        public void Stop()//Method
+        {
+            Console.WriteLine("Stop");
+        }
+    }
+}
--- CodePractice/CodePractice/Program.cs
+++ CodePractice/CodePractice/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using CodePractice;
+using System.Net.Http.Headers;
+using System.Security.AccessControl;
+
+Console.WriteLine("Hello, World!");
+ Car audi=new();
+audi.Drive();
+Console.WriteLine("Type 1 for Stop");
+string userInput=Console.ReadLine();
+
+if (userInput == "1")
+{
+    audi.Stop();
+
+}
+else
+{
+    Console.WriteLine("Driveing");
+}
+
