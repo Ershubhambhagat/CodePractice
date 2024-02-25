92b2991ca24d4dc8eaf64e9af3db1668421c3df9
Shubham Kumar <2090944@cognizant.com>
2024-02-25 19:25:35 +0530

    Day 2 of Practice 4 c# Code Mambers

---
+++ CodePractice/CodePractice/Mambers.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodePractice
+{
+    internal class Mambers
+    {
+        //Mambers -Private Field 
+        private string MemberName;
+        private string jobTitle;
+        private int salary =20000;
+        //Mambers -Public Field 
+        public int age;
+        //mamber - Prop Exposes JobTitle sefely - Property Start with Caps Letter
+        public string JobTitle {
+            get
+            {
+                return jobTitle;
+            }
+            set 
+            {
+                jobTitle = value;
+            } 
+        }
+        //Public member Method- Can be call from Author Classes
+        public void Introduction(bool isFried)
+        {
+            if (isFried)
+            {
+                ShareingPrivateInfo();
+            }
+            else
+            {
+                Console.WriteLine("Hi , My Name is {0} and job title is {1}",MemberName,jobTitle);
+            }
+
+        }
+
+        private void ShareingPrivateInfo()
+        {
+            Console.WriteLine("My Salery  is {0}",salary);
+        }
+
+        //Member constructor
+
+        public Mambers()
+        {
+            MemberName = "Shubham ";
+            jobTitle = "Software Engineer";
+            salary = 100000;
+            Console.WriteLine("Object Created");
+        }
+        //Member -finelazer -destructor
+        ~Mambers()
+        {
+            //clean up statement 
+            Console.WriteLine(  "DEconstruction of Member Object");
+        }
+
+    }
+}
--- CodePractice/CodePractice/Program.cs
+++ CodePractice/CodePractice/Program.cs
@@ -1,46 +1,61 @@
 ﻿using CodePractice;
+using System.Diagnostics.CodeAnalysis;
 using System.Net.Http.Headers;
+using System.Net.Mime;
 using System.Security.AccessControl;
 
 #region Day_1 25/02/2024
 
-Car audi =new("audi");
-Console.WriteLine(audi.GetHP()+"is Hp of this Car");
-Car BMW=new("BMW");
-audi.Deatais();
-BMW.Deatais();
-BMW.Name = audi.Name;
-audi.Drive();
-Console.WriteLine("Type 1 for Stop");
-//string userInput=Console.ReadLine();
-var userInput="1";
-#region if 
-if (userInput == "1")
-{
-    audi.Stop();
-}
-else
-{
-    Console.WriteLine("Driveing");
-}
-#endregion
+//Car audi =new("audi");
+//Console.WriteLine(audi.GetHP()+"is Hp of this Car");
+//Car BMW=new("BMW");
+//audi.Deatais();
+//BMW.Deatais();
+//BMW.Name = audi.Name;
+//audi.Drive();
+//Console.WriteLine("Type 1 for Stop");
+////string userInput=Console.ReadLine();
+//var userInput="1";
+//#region if 
+//if (userInput == "1")
+//{
+//    audi.Stop();
+//}
+//else
+//{
+//    Console.WriteLine("Driveing");
+//}
+//#endregion
 #endregion
 
 
 #region Day_2 25/02/2024
-Car MyCar = new Car();
-Console.WriteLine(MyCar + "is my car 1");
-MyCar.Deatais();
-MyCar.Name = "Something";
-Console.WriteLine(MyCar + "is my car 2");
-MyCar.Deatais();
-MyCar.MaxSpeed = 6884; //Now we can  set 
-Console.WriteLine("my car max speed is " + MyCar.MaxSpeed);
+//Car MyCar = new Car();
+//Console.WriteLine(MyCar + "is my car 1");
+//MyCar.Deatais();
+//MyCar.Name = "Something";
+//Console.WriteLine(MyCar + "is my car 2");
+//MyCar.Deatais();
+//MyCar.MaxSpeed = 6884; //Now we can  set 
+//Console.WriteLine("my car max speed is " + MyCar.MaxSpeed);
 
-//MyCar.Waight = 6884; //read only prop
-Console.WriteLine("my car Waight  is " + MyCar.Waight);
+////MyCar.Waight = 6884; //read only prop
+//Console.WriteLine("my car Waight  is " + MyCar.Waight);
 
 
 
 #endregion
 
+
+
+
+#region Day_2 {2} 25/02/2024
+
+
+Mambers members =new Mambers();
+members.Introduction(true);
+Console.ReadKey();
+
+
+
+#endregion
