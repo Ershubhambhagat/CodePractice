dc52e8d2adc0aed22f34a7b2e89d8fac0aa156be
Shubham Kumar <2090944@cognizant.com>
2024-03-05 14:11:40 +0530

    Find min value

---
+++ CodePractice/Find Minium Number Using Parems/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Security.Cryptography;
+
+class Programe
+{
+
+    static void Main(string[] args)
+    {
+
+        int min= math2(2, 3, 34, 1, 434, 3, 123);
+        Console.WriteLine(  "This is min numnber :{0}",min);
+
+    }
+
+    public static int math2(params int[] numbers)
+    {
+        int min = int.MaxValue;
+        foreach (var number in numbers)
+        {
+            if(number< min)
+            {
+                min = number;
+            }
+
+        }
+        return min;
+    }
+
+}
