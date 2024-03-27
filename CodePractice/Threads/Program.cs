bfba7fdf6ea0d4b39c5e1c8bb5ce6034e6f2d7b2
Shubham Kumar <2090944@cognizant.com>
2024-03-27 12:10:27 +0530

    Theades

---
+++ CodePractice/Threads/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Waiting");
+Console.ReadKey();
+
+int n = 100;
+
+#region Thread
+
+
+foreach (var item in Enumerable.Range(0, n).ToList())
+{
+    new Thread(() =>
+    {
+        Console.WriteLine($"Start Thread number {Thread.CurrentThread.ManagedThreadId} ");
+        Thread.Sleep(1000);
+        Console.WriteLine($"Thread number End{Thread.CurrentThread.ManagedThreadId}");
+    });
+    // }).Start();
+}
+Console.ReadKey();
+Enumerable.Range(0, n).ToList().ForEach(f =>
+{
+    new Thread(() =>
+    {
+        Console.WriteLine($"Start Thread number {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(1000);
+        Console.WriteLine($"Thread number End{Thread.CurrentThread.ManagedThreadId}");
+    });
+    // }).Start();
+
+});
+Console.WriteLine("3 Waiting");
+
+Console.ReadKey();
+#endregion
