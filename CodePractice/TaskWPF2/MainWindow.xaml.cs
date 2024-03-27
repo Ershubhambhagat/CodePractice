d578577a4550ee4086c3fa73536d5ce6fbef039e
Shubham Kumar <2090944@cognizant.com>
2024-03-27 17:29:40 +0530

    WPS and Task

---
+++ CodePractice/TaskWPF2/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskWPF2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine($"Thread NO{Thread.CurrentThread.ManagedThreadId}");
+            
+            HttpClient WebClint=new HttpClient();
+            string html = WebClint.GetStringAsync("https://www.google.com/").Result;
+            MyButton.Content = "Done";
+        }
+    }
+}
